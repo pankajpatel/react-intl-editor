7d675b107c52ec17abca3a512a08880204a51ce5
gabriel <gchang@b-flower.com>
2016-08-24 13:01:27 +0200

    Initial commit

---
+++ app/actions/counter.js
@@ -0,0 +1,42 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+export const SAVE_COUNTER = 'SAVE_COUNTER';
+
+export function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+export function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  };
+}
+
+export function save() {
+  return {
+    type: SAVE_COUNTER
+  };
+}
+
+export function incrementIfOdd() {
+  return (dispatch, getState) => {
+    const { counter } = getState();
+
+    if (counter % 2 === 0) {
+      return;
+    }
+
+    dispatch(increment());
+  };
+}
+
+export function incrementAsync(delay = 1000) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(increment());
+    }, delay);
+  };
+}
